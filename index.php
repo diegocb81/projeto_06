9cc949da6c0cfb5e04d616dadf1180355510ff5a
Diego Barcellos <diegocb81@gmail.com>
2021-01-14 00:21:22 -0300

    Desenvolvendo o header

--- index.php
+++ index.php
@@ -1,12 +1,41 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <title>Projeto 06</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width;initial-scale=1.0;maximum-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/fontawesome.min.css">
 </head>
+
 <body>
-    
+    <div class="icons-social">
+        <a href="https://facebook.com"><i class="fab fa-facebook" aria-hidden="true"></i></a>
+        <a href="https://instagram.com"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+    </div>
+    <!--icons-social-->
+    <section class="main">
+        <div class="center">
+            <header>
+                <div class="logo">
+                    DAST
+                </div>
+                <!--logo-->
+                <nav class="desktop">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Sobre</a></li>
+                        <li><a href="">Contato</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+        <!--center-->
+    </section>
+    <!--main-->
+
 </body>
+
 </html>
